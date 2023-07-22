eae12b0ab72dcc375b8c32b463fca2b4989b7bda
GBergatto <gb.bergatto@gmail.com>
2023-07-22 16:09:59 +0200

    Add script to generate Expr.java with AST classes
    Written in Python instead of Java, as it's just a tool to automate
    boilerplate code

---
+++ jlox/generate_ast.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python
+import os
+
+# script to build Expr.java with all the classes for the Abstract Syntax Tree
+base_name = "Expr"
+rel_path = "com/craftinginterpreters/lox"
+types = [
+   "Binary : Expr left, Token operator, Expr right",
+   "Grouping : Expr expression",
+   "Literal : Object value",
+   "Unary : Token operator, Expr right",
+]
+
+
+def define_type(file, class_name, fields):
+   file.write(f"\n\tstatic class {class_name} extends {base_name} {{\n")
+   file.write(f"\t\t{class_name} ({fields}) {{\n")
+
+   fields = fields.split(", ")
+   for field in fields:
+      name = field.split(" ")[1]
+      file.write(f"\t\t\tthis.{name} = {name};\n")
+   file.write("\t\t}\n\n")
+
+   for field in fields:
+      file.write(f"\t\tfinal {field};\n")
+
+   file.write("\t}\n")
+
+
+# calculate absolute path for the file
+script_dir = os.path.dirname(__file__).rstrip("/.")
+file_path = os.path.join(script_dir, rel_path, f"{base_name}.java")
+
+with open(file_path, "w") as f:
+   # write the base class
+   f.write("package com.craftinginterpreters.lox;\n\n")
+   f.write("import java.util.List;\n\n")
+   f.write(f"abstract class {base_name} {{\n")
+
+   # write subclasses
+   for t in types:
+      class_name = t.split(":")[0].strip()
+      fields = t.split(":")[1].strip()
+      define_type(f, class_name, fields)
+
+   f.write("}\n")
+
+
