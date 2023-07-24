df87bea1a9304e50720ee4f4fe5913368a6996dd
GBergatto <gb.bergatto@gmail.com>
2023-07-24 17:05:17 +0200

    Connect Lox to Parser

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -44,10 +44,15 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    // stop if there was a syntax error.
+    if (hadError) return;
+
+    // for now, print the AST
+    System.out.println(new AstPrinter().print(expression));
   }
 
   // error handling
@@ -60,5 +65,13 @@ public class Lox {
     System.err.println(
       "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 }
