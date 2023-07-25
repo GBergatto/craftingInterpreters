7a505aebfd3033ab3c2104f854880585fa6d4f89
GBergatto <gb.bergatto@gmail.com>
2023-07-25 16:55:09 +0200

    Connect Lox to Interpreter

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -25,7 +27,9 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-    if (hadError) System.exit(65); // exit on error
+    // exit on error
+    if (hadError) System.exit(65); 
+    if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -51,13 +55,19 @@ public class Lox {
     // stop if there was a syntax error.
     if (hadError) return;
 
-    // for now, print the AST
-    System.out.println(new AstPrinter().print(expression));
+    // System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   // error handling
   static void error(int line, String message) {
     report(line, "", message);
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
   private static void report(int line, String where,
