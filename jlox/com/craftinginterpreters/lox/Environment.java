969c25146bb10a8fbeeb4f7d202c4cb405d8959d
GBergatto <gb.bergatto@gmail.com>
2023-07-28 15:12:07 +0200

    Implement cactus stack in Enviroment for scopes

--- jlox/com/craftinginterpreters/lox/Environment.java
+++ jlox/com/craftinginterpreters/lox/Environment.java
@@ -4,12 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+	final Environment enclosing; // reference to parent scope
 	private final Map<String, Object> values = new HashMap<>();
+
+	Environment() { // constructor for global scope
+    enclosing = null;
+  }
+
+	Environment(Environment enclosing) { // local scope
+    this.enclosing = enclosing;
+  }
 
 	Object get(Token name) {
 		if (values.containsKey(name.lexeme)) {
 			return values.get(name.lexeme);
 		}
+
+		// recursively look inside the enclosing scope
+		if (enclosing != null) return enclosing.get(name);
 
 		throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
 	}
@@ -23,6 +35,12 @@ class Environment {
 			values.put(name.lexeme, value);
 			return;
 		}
+
+		if (enclosing != null) {
+			enclosing.assign(name, value);
+			return;
+		}
+
 		throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
 	}
 }
