f0af384c297cca6b6973747c774971db40c867f5
GBergatto <gb.bergatto@gmail.com>
2023-07-28 07:52:25 +0200

    Implement missing visit functions in AstPrinter

--- jlox/com/craftinginterpreters/lox/AstPrinter.java
+++ jlox/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,6 +1,8 @@
 package com.craftinginterpreters.lox;
 
-class AstPrinter implements Expr.Visitor<String> {
+import java.util.List;
+
+class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
 	// visit the first (outer) expression
 	String print(Expr expr) {
 		return expr.accept(this);
@@ -33,15 +35,65 @@ class AstPrinter implements Expr.Visitor<String> {
 		return expr.name.toString();
 	}
 
+	@Override
+	public String visitAssignExpr(Expr.Assign expr) {
+		return parenthesize2("=", expr.name.lexeme, expr.value);
+	}
+
+	@Override
+	public String visitExpressionStmt(Stmt.Expression stmt) {
+		return parenthesize(";", stmt.expression);
+	}
+
+	@Override
+	public String visitVarStmt(Stmt.Var stmt) {
+		if (stmt.initializer == null) {
+			return parenthesize2("var", stmt.name);
+		}
+
+		return parenthesize2("var", stmt.name, "=", stmt.initializer);
+	}
+
+	@Override
+	public String visitPrintStmt(Stmt.Print stmt) {
+		return parenthesize("print", stmt.expression);
+	}
+
 	private String parenthesize(String name, Expr... exprs) {
 		StringBuilder builder = new StringBuilder();
-	builder.append("(").append(name);
+		builder.append("(").append(name);
 		for (Expr expr : exprs) {
 			builder.append(" ");
 			builder.append(expr.accept(this)); // visit recursively the subexpression
 		}
-	builder.append(")");
+		builder.append(")");
 		return builder.toString();
 	}
-}
 
+	private String parenthesize2(String name, Object... parts) {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("(").append(name);
+		transform(builder, parts);
+		builder.append(")");
+
+		return builder.toString();
+	}
+
+	private void transform(StringBuilder builder, Object... parts) {
+		for (Object part : parts) {
+			builder.append(" ");
+		if (part instanceof Expr) {
+				builder.append(((Expr) part).accept(this));
+			} else if (part instanceof Stmt) {
+				builder.append(((Stmt) part).accept(this));
+			} else if (part instanceof Token) {
+				builder.append(((Token) part).lexeme);
+			} else if (part instanceof List) {
+				transform(builder, ((List) part).toArray());
+			} else {
+				builder.append(part);
+			}
+		}
+	}
+}
