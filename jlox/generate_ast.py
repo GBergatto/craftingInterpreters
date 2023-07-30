74a38174f1ec0a42fc10bb3d4c1e777aae5f69d6
GBergatto <gb.bergatto@gmail.com>
2023-07-30 15:02:27 +0200

    Implement conditional execution with if statements

--- jlox/generate_ast.py
+++ jlox/generate_ast.py
@@ -74,6 +74,7 @@ expr_types = [
 stmt_types = [
    "Block : List<Stmt> statements",
    "Expression : Expr expression",
+   "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
    "Print : Expr expression",
    "Var : Token name, Expr initializer",
 ]
