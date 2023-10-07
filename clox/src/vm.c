45e01dba51cca1d1549763e6b4d469f6e82d613e
GBergatto <gb.bergatto@gmail.com>
2023-10-07 09:11:54 +0200

    Implement arithmetic operators

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -36,6 +36,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false) // ensures all statements are in a single scope
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -59,6 +65,12 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD: BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      #undef BINARY_OP
+      case OP_NEGATE: push(-pop()); break;
       case OP_RETURN: {
         printValue(pop());
         printf("\n");
@@ -69,6 +81,7 @@ static InterpretResult run() {
 
   #undef READ_BYTE
   #undef READ_CONSTANT
+  #undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
